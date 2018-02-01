6c9f2db14eb7b8eefeefe6b3b55abd2a1ded64b6
xuleichao <1205448156@qq.com>
2018-02-01 22:58:18 +0800

    define

--- DefBpNN.py
+++ DefBpNN.py
@@ -18,8 +18,15 @@ from matplotlib import pyplot as plt
 def NNtrain(X, y, nnHid_scale, num_passes=2000, study_rate=0.01): #神经网络模型训练
     X_num = len(X) #特征的个数
     y_num = len(y) #标记集的个数
-    #1. 先算隐层输入，2. 然后算隐层输出，3. 然后算输出层输入，4. 然后算输出层输出，
-    #5. 获得损失，然后更新各个参数
+    '''
+    1. 先算隐层输入，2. 然后算隐层输出，
+    3. 然后算输出层输入，4. 然后算输出层输出，
+    5. 获得损失，然后更新各个参数
+    '''
+    Wt_I = np.ones(X_num, nnHid_scale)#输入层权重
+    Gate_H = np.zeros(nnHid_scale)#隐层的阈值
+    Wt_H = np.ones(nnHid_scale, y_num)#隐层的权重
+    Gate_O = np.zeros(y_num)#输出层的阈值
 
     
 if __name__ == '__main__':
