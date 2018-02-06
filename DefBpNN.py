5447156befe8bd6eeaf84c76e045cb739395b80a
xuleichao <1205448156@qq.com>
2018-02-06 21:00:55 +0800

    success

--- DefBpNN.py
+++ DefBpNN.py
@@ -15,6 +15,10 @@ from matplotlib import pyplot as plt
 #nnHid_scale: 隐层的个数
 #num_passes: 训练次数上限
 #study_rate: 学习率
+def sigmoid(lst):
+    y = 1/(1 + np.exp(lst))
+    return y
+
 def NNtrain(X, y, nnHid_scale, num_passes=2000, study_rate=0.01): #神经网络模型训练
     X_num = X.shape[1] #特征的个数
     y_num = 1 #标记集的个数
@@ -29,21 +33,22 @@ def NNtrain(X, y, nnHid_scale, num_passes=2000, study_rate=0.01): #神经网络�
     yuzhi_out = np.zeros(y_num) #输出的阈值
 
     for i in range(num_passes): #迭代更新
-        Hid_out = X.dot(W_input) - yuzhi_hid#隐层输出
-        Out_out = Hid_out.dot(W_hid) - yuzhi_out#输出层输出
-        #开始更新
-        G = Out_out * (1 - Out_out) * (np.array(y) - Out_out) #
-        delta_W_hid = study_rate * G.dot(Hid_out) # 隐层权重的增量
-        delta_yuzhi_out = -study_rate * G#输入端阈值更新增量
-        print(W_hid.shape, G.shape, Hid_out.shape)
-        E = Hid_out * (1 - Hid_out) * np.dot(W_hid, G)#
-        delta_W_input = study_rate * E * np.array(X) #输入端权重更新增量
-        delta_yuzhi_hid = -study_rate * E#隐层阈值更新增量
-        #权重阈值更新
-        W_hid = W_hid + delta_W_hid
-        yuzhi_out = yuzhi_out + delta_yuzhi_out
-        W_input = W_input + delta_W_input
-        yuzhi_hid = yuzhi_hid + delta_yuzhi_hid
+        for x in range(X.shape[0]):
+            Hid_out = sigmoid(X[x].dot(W_input) - yuzhi_hid)#隐层输出
+            Out_out = sigmoid(Hid_out.dot(W_hid) - yuzhi_out)#输出层输出
+            #开始更新
+            G = Out_out * (1 - Out_out) * (np.array(y[x]) - Out_out) #
+            delta_W_hid = study_rate * Hid_out.reshape(nnHid_scale, -1).dot(G) # 隐层权重的增量
+            delta_yuzhi_out = -study_rate * G#输入端阈值更新增量
+            E = Hid_out * (1 - Hid_out) * np.dot(W_hid, G)#
+            delta_W_input = study_rate * (E.reshape(nnHid_scale, -1)\
+                            * np.array(X[x]).reshape(-1, X_num)).T #输入端权重更新增量
+            delta_yuzhi_hid = -study_rate * E#隐层阈值更新增量
+            #权重阈值更新
+            W_hid = W_hid + delta_W_hid.reshape(nnHid_scale, -1)
+            yuzhi_out = yuzhi_out + delta_yuzhi_out
+            W_input = W_input + delta_W_input
+            yuzhi_hid = yuzhi_hid + delta_yuzhi_hid
     return [W_input, W_hid, yuzhi_hid, yuzhi_out]
     
 if __name__ == '__main__':
