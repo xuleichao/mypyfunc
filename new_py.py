c7e7c8decb083a48424018e7e4a64665a2d2857e
xuleichao <1205448156@qq.com>
2018-04-28 17:18:56 +0800

    new

--- new_py.py
+++ new_py.py
@@ -8,14 +8,20 @@ def new():
         path = ''
         f = open(name + '.py','ab')
         bt = '\'\'\'' + '\n\n' + 'by xlc time:' + tm + '\n' + '\'\'\'' + '\n'
-        impt = 'import sys\nsys.path.append(\'G:/Github_codes/mypyfunc\')'
+        impt = '''import sys\n#sys.path.append(\'D:/svn_codes/source/public_fun\')\nimport os
+main_path = os.path.dirname(os.path.abspath(__file__)).replace('\\\\', '/')
+main_path = '/'.join(main_path.split('/')[:-1])
+'''
         main_entry = '''if __name__ == \'__main__\':'''
         f.write((bt + impt + '\n\n' + main_entry).encode('utf-8'))
         f.close()
     else:
         f = open(path + '/' + name + '.py','ab')
         bt = '\'\'\'' + '\n\n' + 'by xlc time:' + tm + '\n' + '\'\'\'' + '\n'
-        impt = 'import sys\nsys.path.append(\'G:/Github_codes/mypyfunc\')'
+        impt = '''import sys\n#sys.path.append(\'D:/svn_codes/source/public_fun\')\nimport os
+main_path = os.path.dirname(os.path.abspath(__file__)).replace('\\\\', '/')
+main_path = '/'.join(main_path.split('/')[:-1])
+'''
         main_entry = '''if __name__ == \'__main__\':'''
         f.write((bt + impt + '\n\n' + main_entry).encode('utf-8'))
         #f.write('\n'.encode('utf-8'))
