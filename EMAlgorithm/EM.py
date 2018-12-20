f5dc4adc1a6962ac37bd7c7200776189b22ecf1b
xuleichao <1205448156@qq.com>
2018-12-20 21:04:47 +0800

    EM_bingo

--- EMAlgorithm/EM.py
+++ EMAlgorithm/EM.py
@@ -7,12 +7,15 @@ import sys
 import os
 main_path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')
 main_path = '/'.join(main_path.split('/')[:-1])
-from load_dd import load_data
+from load_dd import load_dd
 import numpy as np
 from numpy import *
 
 def P_of_mu_sigama(x, mu, sigama):
     # P(x|mu, sigama)
+    #print(x)
+    #print(mu)
+    #print(sigama)
     x = mat(np.array(x))
     mu = mat(np.array(mu))
     sigama = mat(np.array(sigama))
@@ -25,14 +28,15 @@ def P_of_mu_sigama(x, mu, sigama):
     P_forth = 1/((2*np.pi) ** (1/n)) * sigama_det ** 0.5
     P_back = -0.5 * (x - mu) * sigama_reverse * (x - mu).T
     P = P_forth * exp(P_back)
+    #print(x, mu, P, 123456)
     return P
 
-def gamma_i_j_cal(x, mu, sigama, weight_lst, k):
+def gamma_i_j_cal(x, mu_lst, sigama_lst, weight_lst, k):
     # 全概率计算
     P_sum = 0
     sub_prob = []
     for i in range(len(weight_lst)):
-        P = P_of_mu_sigama(x, mu, sigama)
+        P = P_of_mu_sigama(x, mu_lst[i], sigama_lst[i])
         sub_P = weight_lst[i] * P
         P_sum += sub_P
         sub_prob.append(sub_P)
@@ -43,9 +47,10 @@ def gamma_i_j_cal(x, mu, sigama, weight_lst, k):
         i = k
         result[str(i)+'_x'] = (sub_prob[i] / P_sum).tolist()[0][0]
         break
+    #print(result, 1234565)
     return result
         
-def new_mu_vector_cal(datasets, mu, sigama, k, weight_lst):
+def new_mu_vector_cal(datasets, mu_lst, sigama_lst, k, weight_lst):
     '''
     - k: 第几个高斯分量
     '''
@@ -55,9 +60,11 @@ def new_mu_vector_cal(datasets, mu, sigama, k, weight_lst):
         result[str(i) + '_sum_gamma_ij'] = 0
         result[str(i) + '_sum_gamma_ij_x'] = np.zeros(len(datasets[0]))
         break
+    mu = mu_lst[k]
+    sigama = sigama_lst[k]
     for i in datasets:
         for _ in range(len(weight_lst)):
-            gamma_i = gamma_i_j_cal(i, mu, sigama, weight_lst, k)
+            gamma_i = gamma_i_j_cal(i, mu_lst, sigama_lst, weight_lst, k)
             gamma_i_k = gamma_i[str(k)+'_x']
             gamma_ij_x = gamma_i_k * np.array(i)
             result[str(k) + '_sum_gamma_ij'] += gamma_i_k
@@ -78,7 +85,7 @@ def new_sigama_cal(datasets, mu, sigama, k, weight_lst):
     for _ in range(len(weight_lst)):
         i = k
         result[str(i) + '_sum_sigama_ij'] = 0
-        result[str(i) + '_sum_sigama_ij_x'] = np.zeros((len(weight_lst), len(weight_lst)))
+        result[str(i) + '_sum_sigama_ij_x'] = np.zeros((len(datasets[0]), len(datasets[0])))
         break
     for i in datasets:
         gamma_i = gamma_i_j_cal(i, mu, sigama, weight_lst, k)
@@ -139,42 +146,45 @@ def E_M(datasets, mu_lst, gamma_lst, weight_lst):
 
 if __name__ == '__main__':
     # E_M 过程
-    datasets = load_data()
+    datasets = load_dd()
     # 参数初始化
     # 平均值
-    mu_lst = [np.array(datasets[0]), np.array(datasets[10])]
+    mu_lst = [np.array(datasets[5]), np.array(datasets[21]), np.array(datasets[26])]
     # 协方差
-    sigama_lst = [np.array([[1, 0],
-                           [0, 1.9]], dtype='float64'),
-                 np.array([[1, 0],
-                           [0, 1]], dtype='float64')]
+    sigama_lst = [np.array([[0.1, 0],
+                           [0, 0.1]], dtype='float64'),
+                 np.array([[.1, 0],
+                           [0, .1]], dtype='float64'),
+                  np.array([[.1, 0],
+                           [0, .1]], dtype='float64')]
     # 高斯的权重
-    weight_lst = [0.4, 0.6]
-    num = 100 # 迭代次数
+    weight_lst = [0.3333, 0.3333, 0.33333]
+    num = 10 # 迭代次数
     for i in range(num):
-        print(mu_lst)
-        print(sigama_lst)
-        print(weight_lst)
+        #print(mu_lst)
+        #print(sigama_lst)
+        #print(weight_lst)
         new_mu_lst = []
         new_sigama_lst = []
         new_weight_lst = []
+        
         for j in range(len(mu_lst)):
             mu = mu_lst[j]
             sigama = sigama_lst[j]
             weight = weight_lst[j]
-            new_mu = new_mu_vector_cal(datasets, mu, sigama, j, weight_lst)
-            new_sigama = new_sigama_cal(datasets, mu, sigama, j, weight_lst)
-            new_weight = new_weight_cal(datasets, mu, sigama, j, weight_lst)
+            new_mu = new_mu_vector_cal(datasets, mu_lst, sigama_lst, j, weight_lst)
+            new_sigama = new_sigama_cal(datasets, mu_lst, sigama_lst, j, weight_lst)
+            new_weight = new_weight_cal(datasets, mu_lst, sigama_lst, j, weight_lst)
             new_mu_lst.append(list(new_mu.values())[0])
             new_sigama_lst.append(list(new_sigama.values())[0])
             new_weight_lst.append(list(new_weight.values())[0])
         mu_lst = new_mu_lst
         sigama_lst = new_sigama_lst
         weight_lst = new_weight_lst
-        print(mu_lst)
+        #print(mu_lst)
         print(sigama_lst)
-        print(weight_lst)
-        #break
+        #print(weight_lst)
+        break
         
     
     
--- EMAlgorithm/load_dd.py
+++ EMAlgorithm/load_dd.py
@@ -9,8 +9,16 @@ import matplotlib.pyplot as plt
 import numpy as np  
 from sklearn.cluster import KMeans
 from sklearn import datasets
+import pandas as pd
 
 def load_data():
     iris = datasets.load_iris()['data']
     X = iris[:, 2:4] ##表示我们只取特征空间中的后两个维度
     return X
+
+#if __name__ == '__main__':
+def load_dd():
+    data = pd.read_excel('qwer.xlsx')
+    data = data[['密度', '含糖量']]
+    data = np.array(data)
+    return data
